30803bc017d37c1ea149fa5059ad64e5e0c841f2
Hayden Carr <HaydenCarrBusiness@gmail.com>
2024-07-17 10:08:18 +0100

    exit

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,6 @@ public class Main {
                 turn++;
             }
         }
-        PrintBoard.print(board);
     }
 }
 
--- src/WinCheck.java
+++ src/WinCheck.java
@@ -3,39 +3,39 @@ public class WinCheck {
         //Horizontal check
         if (board[0][0] == board[0][1] && board[0][0] == board[0][2] && board[0][0] == player) {
             System.out.println("YOU WIN PLAYER " + player);
-            win = true;
+            System. exit(0);
         }
         if (board[1][0] == board[1][1] && board[0][0] == board[1][2] && board[1][0] == player) {
             System.out.println("YOU WIN PLAYER " + player);
-            win = true;
+            System. exit(0);
         }
         if (board[2][0] == board[2][1] && board[0][0] == board[2][2] && board[2][0] == player) {
             System.out.println("YOU WIN PLAYER " + player);
-            win = true;
+            System. exit(0);
         }
 
         //Vertical Check
         if (board[0][0] == board[1][0] && board[0][0] == board[2][0] && board[0][0] == player) {
             System.out.println("YOU WIN PLAYER " + player);
-            win = true;
+            System. exit(0);
         }
         if (board[0][1] == board[1][1] && board[0][1] == board[2][1] && board[0][1] == player) {
             System.out.println("YOU WIN PLAYER " + player);
-            win = true;
+            System. exit(0);
         }
         if (board[0][2] == board[1][2] && board[0][2] == board[2][2] && board[0][2] == player) {
             System.out.println("YOU WIN PLAYER " + player);
-            win = true;
+            System. exit(0);
         }
 
         //Diagonal check
         if (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] == player) {
             System.out.println("YOU WIN PLAYER " + player);
-            win = true;
+            System. exit(0);
         }
         if (board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] == player) {
             System.out.println("YOU WIN PLAYER " + player);
-            win = true;
+            System. exit(0);
         }
     }
 }
